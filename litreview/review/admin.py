3cc3860984e56f0cf6650e02ffc9948e29929c13
Slb59 <sylvie.bricout2@laposte.net>
2023-04-12 17:38:43 +0200

    add admin settings

--- litreview/review/admin.py
+++ litreview/review/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Ticket, Review, UserFollows
 
-# Register your models here.
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ["title", "description", "user", "time_created"]
+    list_filter = ["title","time_created"]
+    search_fields = ["title", "description"]
+    ordering = ["user", "time_created"]
+    raw_id_fields = ["user"]
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ["ticket","rating","user","headline","time_created"]
+    list_filter = ["ticket","time_created"]
+    ordering = ["user","rating"]
+    raw_id_fields = ["user","ticket"]
+
+@admin.register(UserFollows)
+class UserFollowsAdmin(admin.ModelAdmin):
+    list_display = ["user","followed_user"]
+    raw_id_fields = ["user","followed_user"]
+
+    
+
