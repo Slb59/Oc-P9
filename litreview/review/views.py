bece22d94485ded2c32d3c4c97080631b7cc13d0
Slb59 <sylvie.bricout2@laposte.net>
2023-04-13 13:04:57 +0200

    add ticket_detail view

--- litreview/review/views.py
+++ litreview/review/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Ticket
 
-# Create your views here.
+
+def ticket_detail(request, id):
+    ticket = get_object_or_404(Ticket, id=id)
+
+    return render(request, 'review/ticket/detail.html', {'ticket': ticket})
+
+
+
+# def ticket_list_user(request, user_id):
+#     user = get_object_or_404(User, id=user_id)
+#     tickets = Tickets.objects.filter(user=user)
+
