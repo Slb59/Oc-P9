49cb3d5ac3e64458c9bc6086939086022e80de8f
Slb59 <sylvie.bricout2@laposte.net>
2023-04-13 18:36:51 +0200

    add list_user

--- litreview/review/urls.py
+++ litreview/review/urls.py
@@ -5,5 +5,6 @@ app_name = 'review'
 
 urlpatterns = [
  path('tickets', views.ticket_list, name='ticket_list'),
+ path('tickets/<int:id>/', views.ticket_list_user, name='ticket_list_user'),
  path('ticket/<int:id>/', views.ticket_detail, name='ticket_detail'),
 ]
--- litreview/review/views.py
+++ litreview/review/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.models import User
 from .models import Ticket
 
 
@@ -11,7 +12,9 @@ def ticket_list(request):
     tickets = Ticket.objects.all()
     return render(request,'review/ticket/list.html',{'tickets': tickets})
 
-# def ticket_list_user(request, user_id):
-#     user = get_object_or_404(User, id=user_id)
-#     tickets = Tickets.objects.filter(user=user)
+
+def ticket_list_user(request, id):
+    user = get_object_or_404(User, id=id)
+    tickets = Ticket.objects.filter(user=user)
+    return render(request,'review/ticket/list_user.html',{'tickets': tickets, 'user':user})
 
