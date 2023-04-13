9ba737b78535ab5b15838dec753a458e821e0afa
Slb59 <sylvie.bricout2@laposte.net>
2023-04-13 15:49:17 +0200

    display image of ticket

--- litreview/review/views.py
+++ litreview/review/views.py
@@ -7,7 +7,8 @@ def ticket_detail(request, id):
 
     return render(request, 'review/ticket/detail.html', {'ticket': ticket})
 
-
+def ticket_list(request):
+    pass
 
 # def ticket_list_user(request, user_id):
 #     user = get_object_or_404(User, id=user_id)
