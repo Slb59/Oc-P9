0ad9a4dd434007896fc1a4e7cf991f0fb28198b7
Slb59 <sylvie.bricout2@laposte.net>
2023-04-28 14:40:21 +0200

    change css config

--- litreview/review/views.py
+++ litreview/review/views.py
@@ -8,13 +8,15 @@ def ticket_detail(request, id):
 
     return render(request, 'review/ticket/detail.html', {'ticket': ticket})
 
+
 def ticket_list(request):
     tickets = Ticket.objects.all()
-    return render(request,'review/ticket/list.html',{'tickets': tickets})
+    return render(request, 'review/ticket/list.html', {'tickets': tickets})
 
 
 def ticket_list_user(request, id):
     user = get_object_or_404(User, id=id)
     tickets = Ticket.objects.filter(user=user)
-    return render(request,'review/ticket/list_user.html',{'tickets': tickets, 'user':user})
-
+    return render(request, 'review/ticket/list_user.html',
+                  {'tickets': tickets,
+                   'user': user})
