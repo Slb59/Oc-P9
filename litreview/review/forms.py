5188703cca287d95828788e2593e2d5f562a9b52
Slb59 <sylvie.bricout2@laposte.net>
2023-05-15 17:08:09 +0200

    add_review.html, add the review div

--- litreview/review/forms.py
+++ litreview/review/forms.py
@@ -1,14 +1,51 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Ticket, Review
 
 
 class TicketForm(ModelForm):
+
+    title = forms.CharField(
+        widget=forms.widgets.TextInput(
+            attrs={'placeholder': "Titre"}))
+
+    description = forms.CharField(
+        widget=forms.widgets.Textarea(
+            attrs={
+                'placeholder': "Votre texte",
+                "cols": "100"}))
+    
     class Meta:
         model = Ticket
         fields = ['title', 'description', 'image']
 
 
 class ReviewForm(ModelForm):
+
+    headline = forms.CharField(
+        widget=forms.widgets.TextInput(
+            attrs={'placeholder': "Titre"}))
+    
+    body = forms.CharField(
+        widget=forms.widgets.Textarea(
+            attrs={
+                'placeholder': "Votre critique",
+                "cols": "100"}))
+    
+    rating = forms.ChoiceField(
+        required=True,
+        widget=forms.RadioSelect(attrs={'class': 'rate'}),
+        initial=3,
+        choices=[
+            (0, "0"),
+            (1, "1"),
+            (2, "2"),
+            (3, "3"),
+            (4, "4"),
+            (5, "5")
+        ]
+        )
+    
     class Meta:
         model = Review
         fields = ['headline', 'body']
